6c4fabd066d7bebec08b0e714b6d8338750ca16f
ANDO Yasushi <andyjpn@gmail.com>
2009-11-23 14:05:20 +0900

    handle wheel event

--- content.js
+++ content.js
@@ -1,4 +1,8 @@
 var port = chrome.extension.connect({"name": "content"});
 document.addEventListener('mousemove', function(evt) {
-  port.postMessage({from:'content', position:[evt.clientX, evt.clientY]});
+  port.postMessage({from:'content', type:'pointer', position:[evt.clientX, evt.clientY]});
+}, true);
+
+document.addEventListener('mousewheel', function(evt){
+  port.postMessage({from:'content', type:'wheel'});
 }, true);
